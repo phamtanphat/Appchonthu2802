a1fcad30a6d53e0bfd6333de6926b73c430f2cc9
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-04-20 18:58:33 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/ungdungchonthu2802/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/ungdungchonthu2802/MainActivity.java
@@ -2,12 +2,27 @@ package khoapham.ptp.phamtanphat.ungdungchonthu2802;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imgHinhGoc,imgHinhChon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        imgHinhChon = findViewById(R.id.imageviewHinhchon);
+        imgHinhGoc = findViewById(R.id.imageviewHinhgoc);
+
+        String[] mang = getResources().getStringArray(R.array.Arraythucung);
+        int hinh = getResources().getIdentifier(mang[0],"drawable",getPackageName());
+        imgHinhGoc.setImageResource(hinh);
     }
 }
